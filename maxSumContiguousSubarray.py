060ea0cc1533b9f943f0136189fa9d6ce1f1cf93
amit70 <amit.rajula@gmail.com>
2019-11-10 12:59:12 -0700

    Adding new file

--- maxSumContiguousSubarray.py
+++ maxSumContiguousSubarray.py
@@ -1,5 +1,5 @@
-#Given an integer array nums, find the contiguous subarray (containing at least one number)
-# which has the largest sum and return its sum.
+#Given an integer array nums, find the contiguous subarray
+# (containing at least one number) which has the largest sum and return its sum.
 
 def maxSubArray(A):
     maxsum = cursum = A[0]
