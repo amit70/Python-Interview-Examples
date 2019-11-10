0ccaefa83b5a4b848b06cdf741a3ad64464f7f5d
amit70 <amit.rajula@gmail.com>
2019-11-10 12:53:13 -0700

    Adding new file

---
+++ minSubArrayLen.py
@@ -0,0 +1,15 @@
+#Given an array of n positive integers and a positive integer s, find the minimal
+# length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+def minSubArrayLen(s, nums):
+    l = sum = 0
+    res = len(nums) + 1
+    for i in xrange(len(nums)):
+        sum += nums[i]
+        while sum >= s:
+            res = min(res, i - l + 1)
+            sum -= nums[l]
+            l += 1
+    return res if res <= len(nums) else 0
+
+print minSubArrayLen(7,[2,3,1,2,4,3])
